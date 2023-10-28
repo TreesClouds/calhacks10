0a8f7f184ad08a599e12a339085373b68268409f
Joseph Nguyen <theantisine@gmail.com>
2023-10-28 00:09:39 -0700

    Temp data features

--- Facial Analysis/face_detection.py
+++ Facial Analysis/face_detection.py
@@ -1,9 +1,11 @@
-import cvzone, cv2, base64, asyncio
+import cv2, base64, asyncio, json
 from cvzone.FaceDetectionModule import FaceDetector
-from hume import HumeStreamClient, StreamSocket
+from hume import HumeStreamClient
 from hume.models.config import FaceConfig
 
 HUME_KEY = 'A6wdNigFGRQjP7q3616ICffWKxVwpOTTGB60f7IoFnFZAj1R' # TODO: Gonna have to hide this before we get hacked
+with open('Facial Analysis\increment_data.json', 'r') as f:
+    temp_data = json.load(f)
 
 async def main():
     client = HumeStreamClient(HUME_KEY)
@@ -12,6 +14,7 @@ async def main():
         cap = cv2.VideoCapture(0)
         detector = FaceDetector(minDetectionCon=0.5, modelSelection=0)
         curr_frame_count = 0
+        curr_second_count = 0
 
         while True:
             _, img = cap.read()
@@ -28,7 +31,22 @@ async def main():
                     emotions = sorted(result['face']['predictions'][0]['emotions'], key=lambda x: x['score'], reverse=True)
                     top_emotions = emotions[:5]
                     print([i['name'] for i in top_emotions])
-
+                    
+                    #TODO: Add emotions that need to be tracked to a 2 minute database (for now I'll just use a json)
+                    temp_data[curr_second_count] = [] # ! Insert a list with data of emotion values we should keep track of
+                    with open("Facial Analysis\increment_data.json", "w") as outfile:
+                        json.dump(temp_data, outfile)
+                    
+                    curr_second_count += 1
+                    if curr_second_count == 120: # 2 minutes for now (assuming 60 FPS)
+                        #TODO: Two minute summary check
+                        
+                        
+                        # Wipes data from temp database after checking last 2 mins of data
+                        temp_data = {}
+                        with open("Facial Analysis\increment_data.json", "w") as outfile:
+                            json.dump(temp_data, outfile)
+                        curr_second_count = 0
                     curr_frame_count = 0
                 curr_frame_count += 1
 

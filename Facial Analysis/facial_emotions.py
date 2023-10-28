d6210543971f3bc69c7ba15df81a1ec4aa8eabdc
Alexander DeRouen <alexanderderouen@gmail.com>
2023-10-27 20:39:14 -0700

    Basic Hume code

--- Facial Analysis/facial_emotions.py
+++ Facial Analysis/facial_emotions.py
@@ -1,7 +1,19 @@
 import asyncio
+import json
 
-from hume import HumeStreamClient
-from hume.models.config import BurstConfig
-from hume.models.config import ProsodyConfig
+from hume import HumeStreamClient, StreamSocket
+from hume.models.config import FacemeshConfig
 
-api_key = 'A6wdNigFGRQjP7q3616ICffWKxVwpOTTGB60f7IoFnFZAj1R'
+HUME_KEY = 'A6wdNigFGRQjP7q3616ICffWKxVwpOTTGB60f7IoFnFZAj1R'
+
+async def main():
+    client = HumeStreamClient(HUME_KEY)
+    config = FacemeshConfig()
+    async with client.connect([config]) as socket:
+        fm = open("example.txt", "r")
+        result = await socket.send_facemesh(fm.read())
+        emotions = sorted(result['face']['predictions'][0]['emotions'], key=lambda x: x['score'], reverse=True)
+        top_emotions = emotions[:5]
+        print([i['name'] for i in top_emotions])
+
+asyncio.run(main())

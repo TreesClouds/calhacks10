01bc1f532837e3da7e99f413afdcca2cb0f0b111
Joseph Nguyen <62458797+jspnguyen@users.noreply.github.com>
2023-10-27 21:33:45 -0700

    Add files via upload

--- Facial Analysis/face_detection.py
+++ Facial Analysis/face_detection.py
@@ -1,28 +1,44 @@
-from cvzone.FaceMeshModule import FaceMeshDetector
-import cv2
+import cvzone, cv2, base64, asyncio
+from cvzone.FaceDetectionModule import FaceDetector
+from hume import HumeStreamClient, StreamSocket
+from hume.models.config import FaceConfig
 
-cap = cv2.VideoCapture(0)
+HUME_KEY = 'A6wdNigFGRQjP7q3616ICffWKxVwpOTTGB60f7IoFnFZAj1R'
 
-detector = FaceMeshDetector(staticMode=False, maxFaces=2, minDetectionCon=0.5, minTrackCon=0.5)
+def imageCap():
+    cap = cv2.VideoCapture(0)
+    detector = FaceDetector(minDetectionCon=0.5, modelSelection=0)
+    curr_frame_count = 0
 
-while True:
-    success, img = cap.read()
+    while True:
+        _, img = cap.read()
+        img, bboxs = detector.findFaces(img, draw=False)
 
-    img, faces = detector.findFaceMesh(img, draw=True)
+        if bboxs:
+            if curr_frame_count == 60:
+                _, buffer = cv2.imencode('.jpg', img)
+                jpg_as_text = base64.b64encode(buffer)
+                curr_frame_count = 0
+                break # ! remove this
+            curr_frame_count += 1
 
-    if faces:
-        for face in faces:
-            leftEyeUpPoint = face[159]
-            leftEyeDownPoint = face[23]
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-            leftEyeVerticalDistance, info = detector.findDistance(leftEyeUpPoint, leftEyeDownPoint)
+    cap.release()
+    cv2.destroyAllWindows()
 
-            print(leftEyeVerticalDistance)
+    return jpg_as_text
 
-    cv2.imshow("Image", img)
+async def main():
+    client = HumeStreamClient(HUME_KEY)
+    config = FaceConfig()
+    async with client.connect([config]) as socket:
+        # fm = open("example.txt", "r")
+        result = await socket.send_bytes(imageCap())    
+        emotions = sorted(result['face']['predictions'][0]['emotions'], key=lambda x: x['score'], reverse=True)
+        top_emotions = emotions[:5]
+        print([i['name'] for i in top_emotions])
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+asyncio.run(main())

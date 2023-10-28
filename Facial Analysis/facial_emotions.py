f58443523c446945809e560e67841477a902585b
Joseph Nguyen <62458797+jspnguyen@users.noreply.github.com>
2023-10-27 18:48:25 -0700

    Add files via upload

---
+++ Facial Analysis/facial_emotions.py
@@ -0,0 +1,7 @@
+import asyncio
+
+from hume import HumeStreamClient
+from hume.models.config import BurstConfig
+from hume.models.config import ProsodyConfig
+
+api_key = 'A6wdNigFGRQjP7q3616ICffWKxVwpOTTGB60f7IoFnFZAj1R'
